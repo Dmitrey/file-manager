7e3ad4d3d95f130518abf12a362ea652b01db0dc
Dmitrey <dmitrijp8@gmail.com>
2020-11-15 22:33:28 +0300

    initial

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        URL location = Main.class.getProtectionDomain().getCodeSource().getLocation();
+        String path = location.toString();
+        path = path.replace("file:/","").replace("filec2.jar","");
+        System.out.println(path);
+        File file = new File(path);
+        System.out.println("FILE OR DIRECTORY EXISTS: " + file.exists()+"\n");
+        if(file.exists() && file.isDirectory()) {
+            System.out.println("FILES AMOUNT: " + countFiles(file));//включает в себя сам jar файл программы)))
+        }
+    }
+
+    public static long countFiles(File file){
+        long files_count = 0;
+        File[] arr = file.listFiles();
+        for (File f:arr){
+            if(f.isDirectory())
+                files_count += countFiles(f);
+            else files_count++;
+        }
+        return files_count;
+    }
+}
