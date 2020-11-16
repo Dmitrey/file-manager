7d3cb59c6b429828ce44e46d7cfd9ea1d50245ba
Dmitrey <dmitrijp8@gmail.com>
2020-11-16 09:56:25 +0300

    extension parameter added

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
-        //String separator = System.getProperty("file.separator");
+        String file_extension;
         URL location = Main.class.getProtectionDomain().getCodeSource().getLocation();
         String path = location.toString();
         if(System.getProperty("os.name").equals("Linux"))
@@ -13,6 +13,10 @@ public class Main {
         File file = new File(path);
         System.out.println("FILE OR DIRECTORY EXISTS: " + file.exists()+"\n");
         if(file.exists() && file.isDirectory()) {
+            if (args.length != 0){
+                file_extension = args[0];
+                System.out.println("FILES AMOUNT: " + countFilesWithExtension(file, file_extension));//включает в себя сам jar файл программы)))
+            }else
             System.out.println("FILES AMOUNT: " + countFiles(file));//включает в себя сам jar файл программы)))
         }
     }
@@ -24,6 +28,18 @@ public class Main {
             if(f.isDirectory())
                 files_count += countFiles(f);
             else files_count++;
+        }
+        return files_count;
+    }
+
+    public static long countFilesWithExtension(File file, String extension){
+        long files_count = 0;
+        File[] arr = file.listFiles();
+        for (File f:arr){
+            if(f.isDirectory())
+                files_count += countFilesWithExtension(f,extension);
+            else if(f.getName().endsWith(extension))
+                files_count++;
         }
         return files_count;
     }
